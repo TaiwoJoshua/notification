42298ce475bd956680fe8e97ecf038634aec4bc2
teejayo7 <joshuataiwo07@gmail.com>
2022-05-13 08:32:42 +0100

    I added my files

---
+++ main.js
@@ -0,0 +1,48 @@
+getPermission(function() {
+  var drone = new ScaleDrone('KtJ2qzn3CF3svSFe');
+
+  drone.on('open', function(error) {
+    if (error) return console.error(error);
+    // Join a room called notifications
+    var room = drone.subscribe('notifications');
+    room.on('open', function(error) {
+      if (error) {
+        console.error(error);
+      }
+    });
+    room.on('data', function(data) {
+      Push.create(data.title, {
+        body: data.body,
+        icon: data.icon,
+        timeout: 4000,
+        onClick: function() {
+          this.close();
+        }
+      });
+    });
+  });
+
+  drone.on('close', function(event) {
+    console.log('Connection was closed', event);
+  });
+
+  drone.on('error', function(error) {
+    console.error(error);
+  });
+});
+
+/**
+ * Ask the user for desktop notification permissions (if needed)
+ */
+function getPermission(onGranted) {
+  if (!Notification) {
+    alert('Notifications are not supported');
+  }
+  if (Push.Permission.has()) {
+    onGranted();
+  } else {
+    Push.Permission.request(onGranted, function() {
+      alert('Permission denied');
+    });
+  }
+}
